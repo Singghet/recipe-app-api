88be300cdbd15034fef4d04dc684b61ff7c15f8e
Singhat <senhaiti@hotmail.com>
2023-07-27 19:56:50 +0800

    Added psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator funciton
+"""
+
+def add(x,y):
+    """add x and y and return result."""
+    return x+y
+
+def subtract(x,y):
+    """subtract x from y and return the result."""
+    return 5
