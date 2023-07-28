a47939222345093e294d5fb3b05846951d1a2868
Singhat <senhaiti@hotmail.com>
2023-07-28 16:35:30 +0800

    Test Github Action6

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,13 @@
 """
-Calculator funciton
+Calculator functions
 """
 
-def add(x,y):
-    """add x and y and return result."""
-    return x+y
 
-def subtract(x,y):
-    """subtract x from y and return the result."""
-    return 5
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
